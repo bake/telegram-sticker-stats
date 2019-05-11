38efcdec5e9c5c262b8df0a6c958fe9c1d7925c9
bake <bake@192k.pw>
2019-05-11 15:22:24 +0200

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Sticker Stats
+
+A simple script to extract the usage of a Telegram sticker set as CSV that can be plotted.
+
+Clone this repository, [register an application](https://telethon.readthedocs.io/en/latest/basic/signing-in.html#signing-in), copy `.env.example` to `.env` and fill in `API_ID` and `API_HASH`.
+
+```bash
+$ ./sticker.py name > name.csv
+$
+```
+
+![plot](plot.png)
---
+++ sticker.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import sys
+import time
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from telethon import TelegramClient, sync, events
+
+load_dotenv()
+
+client = TelegramClient('anon', os.getenv('API_ID'), os.getenv('API_HASH'))
+client.start()
+
+print('date', 'usage', 'installed', 'removed', sep=',')
+
+
+@client.on(events.NewMessage(chats='Stickers', pattern='^Stats for the sticker pack (.+?) for'))
+async def handler(event):
+    m = re.search(
+        r'([0-9]{2}\/[0-9]{2}\/[0-9]{4}):\n+Usage: ([0-9]+)\nInstalled: ([0-9]+)\nRemoved: ([0-9]+)\n',
+        event.message.message,
+    )
+    date = datetime.strptime(m.group(1), '%m/%d/%Y')
+    print(date.date(), m.group(2), m.group(3), m.group(4), sep=',')
+
+    if date.date() == datetime.now().date():
+        await client.disconnect()
+
+client.send_message('Stickers', '/cancel')
+client.send_message('Stickers', '/packstats')
+client.send_message('Stickers', sys.argv[1])
+time.sleep(1)
+
+for days in range(356, -1, -1):
+    date = datetime.now() - timedelta(days)
+    client.send_message('Stickers', date.strftime('%m/%d/%Y'))
+
+client.run_until_disconnected()
